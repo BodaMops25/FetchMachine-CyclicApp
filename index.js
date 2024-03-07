7717c8ec0f0fbb1aa06d25674c2bb68375c86f99
BodaMops25 <bodabodamops@gmail.com>
2024-03-07 14:22:05 +0100

    fixing commit

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Cyclic testing!<br>');
     res.write('Testing env variables:<br>');
-    res.write(process.env.TESTVAL);
+    res.write(process.env.TESTVAR);
     res.end();
 }).listen(process.env.PORT || 3000);
