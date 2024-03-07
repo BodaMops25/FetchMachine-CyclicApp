204efe26bad9c6e1b7bdc953d77950d5ddaa0333
BodaMops25 <bodabodamops@gmail.com>
2024-03-07 14:41:28 +0100

    fixes

--- index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Cyclic testing!<br>');
-    res.write('Testing env variables:<br>');
-    res.write(process.env.TESTVAR);
-    res.end();
-}).listen(process.env.PORT || 3000);
