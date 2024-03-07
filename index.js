d7747538a86503f83ad50b92386da8081ed8a7de
BodaMops25 <bodabodamops@gmail.com>
2024-03-07 14:18:47 +0100

    commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Cyclic testing!');
+    res.write('Cyclic testing!<br>');
+    res.write('Testing env variables:<br>');
+    res.write(process.env.TESTVAL);
     res.end();
 }).listen(process.env.PORT || 3000);
